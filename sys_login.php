54688081c555c3ae22eef3b14abf1381f919ab01
jakehu <jakehu1991@sina.cn>
2015-04-07 10:37:33 +0800

    Update Login File

--- sys_login.php
+++ sys_login.php
@@ -10,7 +10,7 @@
   <!-- Bootstrap 3.3.2 -->
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" >
+  <link href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" >
   <!-- Theme style -->
   <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
   <!-- iCheck -->
